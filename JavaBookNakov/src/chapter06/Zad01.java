b0a89066dac8f112d1588310dfc3d4e85642bff5
Krachunov <Krachunov@Krachunov-PC>
2015-11-10 10:21:45 +0200

    chapter 6 zad 10

--- JavaBookNakov/src/chapter06/Zad01.java
+++ JavaBookNakov/src/chapter06/Zad01.java
@@ -1,13 +1,14 @@
 package chapter06;
+
 import java.util.Scanner;
 
 public class Zad01 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int x=scanner.nextInt();
-		
-		for(int a=0;a<=x;a++){
+		int x = scanner.nextInt();
+
+		for (int a = 0; a <= x; a++) {
 			System.out.println(a);
 		}
 	}
--- JavaBookNakov/src/chapter06/Zad10.java
+++ JavaBookNakov/src/chapter06/Zad10.java
@@ -1,23 +1,27 @@
 package chapter06;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class Zad10 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		System.out.print("Enter the value: ");
-		int x=scn.nextInt();
-		
+		int x = scn.nextInt();
+
 		int array[][] = new int[x][x];
-		for (int i=0;i<=x-1;i++){
-			for(int j=0;j<=x-1;j++){
-				System.out.println("Enter the row");
-				array[i][j]=scn.nextInt();
-//				System.out.println(java.util.Arrays.toString(array[i][j]));
+
+		for (int i = 1; i < x; i++) {
+			for (int j = i; j < x; j++) {
+				array[i][j] = j;
 			}
 		}
-		System.out.println(java.util.Arrays.toString(array));
+		for (int i = 0; i < array.length; i++) {
+			System.out.println();
+			for (int j = 0; j < array.length; j++) {
+				System.out.print(array[i][j]);
+			}
+		}
 	}
 
 }
